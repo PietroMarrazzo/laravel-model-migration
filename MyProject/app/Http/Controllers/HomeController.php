d528280f1207b26db91b76b194a98c7757009e87
Pietro <marrazzo.pietroalfonso@gmail.com>
2021-01-19 15:11:17 +0100

    cant see data

--- MyProject/app/Http/Controllers/HomeController.php
+++ MyProject/app/Http/Controllers/HomeController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Car;
 
 class HomeController extends Controller
 {
     //
     public function index() {
         
+        // prendere data dal DB
+        $cars = Car::all();
+        dd($cars);
+
+
         return view('home');
     }
 }
